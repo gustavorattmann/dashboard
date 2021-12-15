fdcd774fbe284729a280f47925fe15109c4760c3
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-12-15 00:13:28 -0300

    Importado rotas para ser carregado no servidor

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,16 @@ require('dotenv').config();
 const env = process.env;
 
 import express from 'express';
-
-import { Router, Request, Response } from 'express';
+import routes from './routes/index';
 
 const cors = require('cors');
 
 const app = express();
 
-const route = Router();
-
 app.use(cors());
 app.use(express.json());
 
-route.get('/', async (req, res, next) => {
-	res.status(200).send(env.APP_NAME);
-});
-
-app.use(route);
+app.use(routes);
 
 app.listen(env.APP_PORT, () => {
 	console.log(`Servidor está rodando em ${env.APP_PROTOCOL}://${env.APP_URL}:${env.APP_PORT}`);
